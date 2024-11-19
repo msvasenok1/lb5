a8b0923d6a94a8442c007f824eac433f04a25f89
msvasenok1 <glupie.uchitelya@gmail.com>
2024-11-19 13:07:41 +0300

    last

---
+++ Git tipa.py
@@ -0,0 +1,46 @@
+import random
+
+#5476545
+def welcome():
+    print(
+        "Добро пожаловать в игру с камнями!\nПравила игры:\nКаждый игрок по очереди берет 1, 2 или 3 камня.\nВыигрывает тот, кто оставляет последний камень сопернику.")
+    print("Всего камней: ", stones)
+
+
+def user(stones):
+    while True:
+        try:
+            move = int(input("Сколько камней вы хотите взять (1, 2 или 3)? "))
+            if move in [1, 2, 3] and move <= stones:
+                return move
+            else:
+                print("Ошибка: Вы можете взять только 1, 2 или 3 камня, и не больше, чем осталось.")
+        except ValueError:
+            print("Ошибка: Пожалуйста, введите целое число.")
+
+
+def computer(stones):
+    if stones >= 4:
+        return min(3, stones)
+    else:
+        return stones
+
+
+stones = random.randint(4, 30)
+welcome()
+
+while stones > 0:
+    user_move = user(stones)
+    stones -= user_move
+    print(f"Вы взяли {user_move} камней. Осталось {stones} камней.")
+
+    if stones == 0:
+        print("Поздравляем! Вы выиграли!")
+        break
+
+    computer_move = computer(stones)
+    stones -= computer_move
+    print(f"Компьютер взял {computer_move} камней. Осталось {stones} камней.")
+
+    if stones == 0:
+        print("Компьютер выиграл! Попробуйте снова.")
